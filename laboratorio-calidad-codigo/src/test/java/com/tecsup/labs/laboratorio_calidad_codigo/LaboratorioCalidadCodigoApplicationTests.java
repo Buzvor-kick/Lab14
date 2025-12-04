e702459a50aa409e01a174cfc5f7e981e324197a
Buzvor-kick <cusirramosvalentin@gmail.com>
2025-12-03 23:10:58 -0500

    Primer commit

---
+++ laboratorio-calidad-codigo/src/test/java/com/tecsup/labs/laboratorio_calidad_codigo/LaboratorioCalidadCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecsup.labs.laboratorio_calidad_codigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioCalidadCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
